eb45edf0d372b7360a4ed87a0a64f9747c0a288c
raahinn <rahinn11@gmail.com>
2020-09-10 02:45:42 +0600

    Connecting with firebase, chatroom class, adding chats.

---
+++ scripts/chat.js
@@ -0,0 +1,33 @@
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// update a username
+// updateing the room
+
+class Chatroom {
+    constructor(room, username) {
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+    }
+    async addChat(message) {
+        // formate a chat object
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        // save the chat document
+        const response = await this.chats.add(chat);
+        return response;
+    }
+}
+
+const chatroom = new Chatroom('gaming', 'chaw');
+// console.log(chatroom);
+
+chatroom.addChat('Assalamualikum')
+    .then(() => console.log('chat added'))
+    .catch(err => console.log(err));
+
